ef7499ddb1df28bb6f127125ea6d24458bcc369b
Myles C. Maxfield <mmaxfield@apple.com>
2022-06-05 21:26:55 -0700

    Delay system font keyword resolution

--- Source/WebCore/platform/graphics/SystemFontDatabase.h
+++ Source/WebCore/platform/graphics/SystemFontDatabase.h
@@ -37,7 +37,7 @@ public:
     WEBCORE_EXPORT static SystemFontDatabase& singleton();
 
     enum class FontShorthand {
-        // This needs to be kept in sync with CSSValue.
+        // This needs to be kept in sync with CSSValue and CSSPropertyParserHelpers::lowerFontShorthand().
         Caption,
         Icon,
         Menu,
